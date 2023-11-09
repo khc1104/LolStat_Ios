b9290363616861e15b630266773c3145c4dab6ca
khc1104 <hck1104@naver.com>
2023-11-09 18:06:20 +0900

    Initial Commit

---
+++ LolStat_Ios/LolStat_IosApp.swift
@@ -0,0 +1,17 @@
+//
+//  LolStat_IosApp.swift
+//  LolStat_Ios
+//
+//  Created by 권희철 on 2023/11/09.
+//
+
+import SwiftUI
+
+@main
+struct LolStat_IosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
